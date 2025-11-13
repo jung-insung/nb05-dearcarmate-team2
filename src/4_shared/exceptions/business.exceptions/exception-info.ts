fa2a6ecd0609c5bb4479f240076ba0de607cc878
gyeongyeonk <gyeongyeonk@gyeongyeonkui-MacBookAir.local>
2025-11-13 13:25:25 +0900

    [fix]이름검증 메소드로

--- src/4_shared/exceptions/business.exceptions/exception-info.ts
+++ src/4_shared/exceptions/business.exceptions/exception-info.ts
@@ -1,30 +1,38 @@
 export enum BusinessExceptionType {
+  BAD_REQUEST,
   EMPLOYEENUMBER_TOO_LONG,
   INVALID_CAR_NUMBER,
   INVALID_MILEAGE,
   INVALID_PRICE,
-  BAD_REQUEST,
+  NAME_TOO_LONG,
 }
 
-export const BusinessExceptionTable: Record<BusinessExceptionType, {statusCode: number, message: string}> = {
+export const BusinessExceptionTable: Record<
+  BusinessExceptionType,
+  { statusCode: number; message: string }
+> = {
+  [BusinessExceptionType.BAD_REQUEST]: {
+    statusCode: 400,
+    message: "잘못된 요청입니다.",
+  },
   [BusinessExceptionType.EMPLOYEENUMBER_TOO_LONG]: {
     statusCode: 404,
     message: "비밀번호가 너무 깁니다.",
   },
   [BusinessExceptionType.INVALID_CAR_NUMBER]: {
     statusCode: 404,
-    message: "존재하지 않는 차량번호입니다."
+    message: "존재하지 않는 차량번호입니다.",
   },
   [BusinessExceptionType.INVALID_MILEAGE]: {
     statusCode: 404,
-    message: "입력하신 주행 거리가 비정상적입니다."
+    message: "입력하신 주행 거리가 비정상적입니다.",
   },
   [BusinessExceptionType.INVALID_PRICE]: {
     statusCode: 404,
-    message: "가격은 0이상이어야 합니다."
+    message: "가격은 0이상이어야 합니다.",
   },
-  [BusinessExceptionType.BAD_REQUEST]: {
-    statusCode: 400,
-    message: "잘못된 요청입니다."
-  }
-}
+  [BusinessExceptionType.NAME_TOO_LONG]: {
+    statusCode: 404,
+    message: "이름은 최대 10자까지 입력해주세요.",
+  },
+};
