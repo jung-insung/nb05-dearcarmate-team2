8daff8e30fe7b5d7c9756c37c73285f0795e8078
jung-insung <yon9170@naver.com>
2025-11-14 13:45:23 +0900

    [feat][fix] router, 컨트롤러 추가 및 index,server, injector 등 수정

--- src/injector.ts
+++ src/injector.ts
@@ -1,12 +1,17 @@
+import { PrismaClient } from "@prisma/client";
 import { CorsMiddleware } from "./1_inbound/middleware/cors.middleware";
 import { GlobalErrorMiddleware } from "./1_inbound/middleware/global-error.middleware";
 import { JsonMiddleware } from "./1_inbound/middleware/json.middleware";
 import { LoggerMiddleware } from "./1_inbound/middleware/logger.middleware";
+import { UserRouter } from "./1_inbound/router/user.router";
+import { UserRepo } from "./3_outbound/repos/user.repo";
 import { ConfigUtil } from "./4_shared/utils/config.util";
 import { Server } from "./server";
+import { UserService } from "./2_domain/services/user.service";
+import { UserController } from "./1_inbound/controller/user.controller";
 
 export class Injector {
-  private _server : Server;
+  private _server: Server;
 
   constructor() {
     this._server = this.injectDeps();
@@ -15,13 +20,25 @@ export class Injector {
   get server() {
     return this._server;
   }
-  injectDeps() : Server {
+  injectDeps(): Server {
+    const prisma = new PrismaClient();
+
     const configUtil = new ConfigUtil();
     const corsMiddleware = new CorsMiddleware(configUtil);
     const globalErrorMiddleware = new GlobalErrorMiddleware(configUtil);
     const jsonMiddleware = new JsonMiddleware(configUtil);
     const loggerMiddleware = new LoggerMiddleware(configUtil);
+
+    const userRepo = new UserRepo(prisma);
+
+    const userService = new UserService(userRepo);
+
+    const userController = new UserController(userService);
+
+    const userRouter = new UserRouter(userController);
+
     return new Server(
+      userRouter,
       configUtil,
       corsMiddleware,
       loggerMiddleware,
