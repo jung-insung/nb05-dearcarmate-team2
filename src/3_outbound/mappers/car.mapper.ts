c78bcc8b6bc2e91db8672ede9634dcbdbaac06f2
918-jihye <wlgp181004@gmail.com>
2025-11-17 16:20:18 +0900

    [feat] car mapper 구현

---
+++ src/3_outbound/mappers/car.mapper.ts
@@ -0,0 +1,15 @@
+import { CarEntity, PersistCarRecord } from "../../2_domain/entities/car/car.entity";
+
+export class CarMapper {
+  static toPersistEntity(record: PersistCarRecord): CarEntity {
+    return CarEntity.fromPersistence(record);
+  }
+
+  static toCreateData(entity: CarEntity) {
+    return entity.toCreateData();
+  }
+
+  static toUpdateData(entity: CarEntity) {
+    return entity.toUpdateData();
+  }
+}
