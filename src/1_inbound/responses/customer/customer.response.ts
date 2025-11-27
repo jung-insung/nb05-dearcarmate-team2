a82ec2322fad3b9df4d8a5b2fa83ff70da224d8a
gyeongyeonk <gyeongyeonk@gyeongyeonkui-MacBookAir.local>
2025-11-27 14:27:54 +0900

    [fix]성별 안나옴, 매핑 수정

--- src/1_inbound/responses/customer/customer.response.ts
+++ src/1_inbound/responses/customer/customer.response.ts
@@ -1,10 +1,41 @@
+export type GenderLabel = "남" | "여";
+
+export type AgeGroupLabel =
+  | "10대"
+  | "20대"
+  | "30대"
+  | "40대"
+  | "50대"
+  | "60대"
+  | "70대"
+  | "80대";
+
+export type RegionLabel =
+  | "서울"
+  | "경기"
+  | "인천"
+  | "강원"
+  | "충북"
+  | "충남"
+  | "세종"
+  | "대전"
+  | "전북"
+  | "전남"
+  | "광주"
+  | "경북"
+  | "경남"
+  | "대구"
+  | "울산"
+  | "부산"
+  | "제주";
+
 export interface CustomerResponseDto {
   id: number;
   name: string;
-  gender: string;
+  gender: GenderLabel;
   phoneNumber: string;
-  ageGroup?: string;
-  region?: string;
+  ageGroup?: AgeGroupLabel;
+  region?: RegionLabel;
   email: string;
   memo?: string;
   contractCount: number;
